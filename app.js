64bbba8424710a144fa65c410f00fdd2b2324f38
karta2501076 <94138209+karta2501076@users.noreply.github.com>
2026-03-21 19:22:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  databaseURL: "https://rjpq-tool-2ee82-default-rtdb.firebaseio.com/",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT_ID.appspot.com",
   messagingSenderId: "YOUR_ID",
